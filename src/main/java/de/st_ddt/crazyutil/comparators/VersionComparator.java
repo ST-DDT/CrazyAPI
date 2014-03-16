6e239fc6d48ba38ff45a4ce48e73c406422cc32a
ST-DDT <ST-DDT@gmx.de>
2014-03-16 09:54:07 +0100

    Fixed VersionComparator

--- src/main/java/de/st_ddt/crazyutil/comparators/VersionComparator.java
+++ src/main/java/de/st_ddt/crazyutil/comparators/VersionComparator.java
@@ -105,34 +105,34 @@ public class VersionComparator implements Comparator<String>
 			{
 				switch (cha)
 				{
-					case 0:
+					case '0':
 						res += 0;
 						break;
-					case 1:
+					case '1':
 						res += 1;
 						break;
-					case 2:
+					case '2':
 						res += 2;
 						break;
-					case 3:
+					case '3':
 						res += 3;
 						break;
-					case 4:
+					case '4':
 						res += 4;
 						break;
-					case 5:
+					case '5':
 						res += 5;
 						break;
-					case 6:
+					case '6':
 						res += 6;
 						break;
-					case 7:
+					case '7':
 						res += 7;
 						break;
-					case 8:
+					case '8':
 						res += 8;
 						break;
-					case 9:
+					case '9':
 						res += 9;
 						break;
 					default:
