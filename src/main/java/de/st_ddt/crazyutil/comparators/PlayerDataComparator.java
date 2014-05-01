9362a1a2a75c74d63103f32590159876db4127d2
ST-DDT <ST-DDT@gmx.de>
2014-05-01 10:16:19 +0200

    Refactored Comparators

---
+++ src/main/java/de/st_ddt/crazyutil/comparators/PlayerDataComparator.java
@@ -0,0 +1,9 @@
+package de.st_ddt.crazyutil.comparators;
+
+import java.util.Comparator;
+
+import de.st_ddt.crazyutil.datas.PlayerData;
+
+public interface PlayerDataComparator<S extends PlayerData> extends Comparator<S>
+{
+}
